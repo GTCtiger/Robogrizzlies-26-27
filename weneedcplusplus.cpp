96b1b6e866bd9993d3de02d3cf1014672e6a5994
chillzz <jadenpan124@gmail.com>
2026-09-21 14:49:42 -0500

    Update weneedcplusplus.cpp

--- weneedcplusplus.cpp
+++ weneedcplusplus.cpp
@@ -1,1 +1,9 @@
+#include <iostream>
+#include <unordered_map>
+#define int long long
+using namespace std;
 
+int main() {
+  unordered_map<int, int> hashmap_that_gavin_had_to_implement_himself_on_vulkan_LOL;
+  cout << "you need to try and avoid the white baddies - Gavin Chiam" << endl;
+}
