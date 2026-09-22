1fdae98e504d281ddb2df5fdef54d10203455594
chillzz <jadenpan124@gmail.com>
2026-09-22 08:30:03 -0500

    Update hehe.cpp

--- hehe.cpp
+++ hehe.cpp
@@ -4,6 +4,6 @@
 using namespace std;
 
 int main() {
-  unordered_map<int, int> hashmap_that_gavin_had_to_implement_himself_on_vulkan_LOLsss;
+  unordered_map<int, int> hashmap_that_gavin_had_to_implement_himself_on_vulkan_LOL;
   cout << "you need to try and avoid the white baddies - Gavin Chiam" << endl;
 }
